2cfd0d43f8336f8e446f9ba071625e75b2786bbf
Fagner Pereira Rosa <fagnerfpr@gmail.com>
2018-09-05 07:39:55 -0300

    add pt-br as default language

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904093940) do
+ActiveRecord::Schema.define(version: 20180905101113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "institutions", force: :cascade do |t|
     t.string "name"
-    t.string "cnpj"
+    t.integer "cnpj"
     t.string "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_institutions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 20180904093940) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "institutions", "users"
 end
