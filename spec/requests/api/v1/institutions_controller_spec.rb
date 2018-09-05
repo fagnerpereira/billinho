a87718b580bc65f68374a874ef05dd1e8dfdd330
Fagner Pereira Rosa <fagnerfpr@gmail.com>
2018-09-05 07:39:27 -0300

    add institutions tests

--- spec/requests/api/v1/institutions_controller_spec.rb
+++ spec/requests/api/v1/institutions_controller_spec.rb
@@ -1,17 +1,101 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::InstitutionsController, type: :request do
-  let(:user) { User.create(email: 'alice@example.com') }
   let(:json) { JSON.parse(response.body) }
+  let(:user) { User.create(email: 'alice@example.com') }
+  let(:institution_attributes) do
+    {
+      name: 'FIAP',
+      cnpj: 123456789,
+      kind: 'Universidade'
+    }
+  end
 
-  describe "GET /show" do
+  describe 'POST /create' do
     context 'success' do
       before do
-        get api_v1_institution_path(id: 123), params: { access_token: user.access_token }
+        post api_v1_institutions_path,
+          params: {
+              institution: institution_attributes,
+              access_token: user.access_token
+            }
+      end
+
+      it { expect(response).to have_http_status(:created) }
+      it { expect(json['institution']['name']).to eq(institution_attributes[:name]) }
+      it { expect(json['institution']['cnpj']).to eq(institution_attributes[:cnpj]) }
+      it { expect(json['institution']['kind']).to eq(institution_attributes[:kind]) }
+    end
+
+    context 'fail' do
+      context 'empty values' do
+        before do
+          post api_v1_institutions_path,
+            params: {
+                institution: { name: '' },
+                access_token: user.access_token
+              }
+        end
+
+        it { expect(response).to have_http_status(:unprocessable_entity) }
+        it { expect(json).to include('Nome não pode ficar em branco') }
+        it { expect(json).to include('CNPJ não pode ficar em branco') }
+        it { expect(json).to include('Tipo não pode ficar em branco') }
+      end
+
+      context 'duplicated values' do
+        before do
+          user.institutions.create(institution_attributes)
+
+          post api_v1_institutions_path,
+            params: {
+                institution: institution_attributes,
+                access_token: user.access_token
+              }
+        end
+
+        it { expect(response).to have_http_status(:unprocessable_entity) }
+        it { expect(json).to include('Nome já está em uso') }
+        it { expect(json).to include('CNPJ já está em uso') }
+        it { expect(json).to include('Tipo já está em uso') }
+      end
+
+      context 'invalid values' do
+        let(:institution_attributes) do
+          {
+            name: 'FIAP',
+            cnpj: 'abc',
+            kind: 'Azeitonas'
+          }
+        end
+
+        before do
+          post api_v1_institutions_path,
+            params: {
+                institution: institution_attributes,
+                access_token: user.access_token
+              }
+        end
+
+        it { expect(response).to have_http_status(:unprocessable_entity) }
+        it { expect(json).to include("Tipo #{institution_attributes[:kind]} inválido, valores válidos: Universidade, Escola ou Creche") }
+        it { expect(json).to include("CNPJ #{institution_attributes[:cnpj]} não é um número") }
+      end
+    end
+  end
+
+  describe 'GET /show' do
+    let(:institution) { user.institutions.create(institution_attributes) }
+
+    context 'success' do
+      before do
+        get api_v1_institution_path(institution), params: { access_token: user.access_token }
       end
 
       it { expect(response).to have_http_status(:ok) }
-      it { expect(json['user']['email']).to include(user.email) }
+      it { expect(json['institution']['name']).to eq(institution.name) }
+      it { expect(json['institution']['cnpj']).to eq(institution.cnpj) }
+      it { expect(json['institution']['kind']).to eq(institution.kind) }
     end
 
     context 'fail' do
